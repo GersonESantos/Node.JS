88e7f9c10130c9a9e9e6bdef53ae2aae9fe1a589
GersonESS <gebhsantos@gmail.com>
2025-01-27 19:08:34 -0300

    Update app.js

--- 05/app.js
+++ 05/app.js
@@ -1,3 +1,11 @@
+// This code creates a server that listens on port 3000.
 var http = require('http');
-http.createServer().listen(3000);
+http.createServer(
+    function(req, res) 
+    {
+        res.end('Hello World!');
+    }
+                    ).listen(3000);
+
+
 console.log('Server running on port 3000');
