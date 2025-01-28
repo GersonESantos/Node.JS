7c6a60983b3752b6ea1187f4e54836001c907111
GersonESS <gebhsantos@gmail.com>
2025-01-27 21:43:35 -0300

    Update index.js

--- 08/index.js
+++ 08/index.js
@@ -10,6 +10,13 @@ app.get('/', (req, res) => {
     res.send('sobre o que eu vou falar     ' );
   });
 
+  app.get('/ola', (req, res) => {
+    res.send('ola' );
+  });
+
+  app.get('/ola/:cargo/:nome/:cor', (req, res) => {
+    res.send('<h1>ola '+req.params.nome+"</h1>"+'<h2>seu cargo é: '+req.params.cargo+"</h2>"+'<h3>sua cor favorita é: '+req.params.cor+"</h3>");
+  });
 
 app.listen(8080, () => {
     console.log('Rodando app listening at http://localhost:8080');
