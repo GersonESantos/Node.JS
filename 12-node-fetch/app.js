55795a5579ed4dbddc45e3ef29905a5bc1ed0434
GersonESS <gebhsantos@gmail.com>
2025-02-09 20:44:31 -0300

    a

---
+++ 12-node-fetch/app.js
@@ -0,0 +1,21 @@
+// Importar os módulos
+var express = require('express');
+var fetch = require('node-fetch');
+
+// App
+var app = express();
+
+// Rota
+app.get('/', function(req, res){
+
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(resposta => resposta.json())
+    .then(resposta => console.table(resposta))
+
+    res.end();
+});
+
+// Servidor
+listen(3000, () => {
+    console.log('Servidor rodando na porta 3000');
+});
