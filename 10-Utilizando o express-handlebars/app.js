436d8cd5fcb5445641d4d1439fba323224c856e9
GersonESS <gebhsantos@gmail.com>
2025-02-09 12:47:48 -0300

    a

--- 10-Utilizando o express-handlebars/app.js
+++ 10-Utilizando o express-handlebars/app.js
@@ -1,18 +1,20 @@
+// Importar módulo express
 var express = require('express');
-var exphbs = require('express-handlebars');
+
+
+const { engine } = require('express-handlebars');
 
 const app = express();
 
-app.engine('handlebars', exphbs({defaultLayout: 'principal'}));
+
+app.engine('handlebars', engine());
+
 app.set('view engine', 'handlebars');
-//app.set('views', './views');
+app.set('views', './views');
 
 app.get('/', (req, res) => {
     res.render('inicio');
 });
 
-app.get('/sobre', (req, res) => {
-    res.render('sobre');
-});
 
 app.listen(3000);
