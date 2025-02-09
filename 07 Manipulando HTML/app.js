730b05e46029896775b79f20f5aefb21404fc628
GersonESS <gebhsantos@gmail.com>
2025-02-09 04:30:01 -0300

    a

--- 07 Manipulando HTML/app.js
+++ 07 Manipulando HTML/app.js
@@ -2,8 +2,8 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-  res.end()
+  res.sendFile(__dirname + '/pagina.html')
+  
 })
 
 const port = 3000
