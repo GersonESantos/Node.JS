162c48376e93841c894fa4baeebe0a60327fd676
GersonESS <gebhsantos@gmail.com>
2025-02-09 18:52:12 -0300

    a

--- 11-express-handlebars (partials, if e each)/app.js
+++ 11-express-handlebars (partials, if e each)/app.js
@@ -22,8 +22,23 @@ app.get('/sobre', (req, res) => {
 app.get('/formulario', (req, res) => {
     res.render('formulario');
 });
+app.get('/laco', (req, res) => {
+    res.render('laco', {gostaDeNode: false});
+});
+app.get('/foreach', (req, res) => {
 
+    let pessoas = [
 
+        {nome: 'João', idade: 25},
+        {nome: 'Maria', idade: 30},
+        {nome: 'José', idade: 28},
+        {nome: 'Ana', idade: 35},
+        {nome: 'Pedro', idade: 40},
+        {nome: 'Paula', idade: 22},
+        {nome: 'Carlos', idade: 33}
+    ];
+    res.render('foreach', {dados: pessoas});
+});
 
 app.listen(8080, () => {
     console.log('Rodando app listening at http://localhost:8080');
