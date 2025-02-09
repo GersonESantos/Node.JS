df69fb847f8c9e317b688c516f4951672728593e
GersonESS <gebhsantos@gmail.com>
2025-02-09 05:45:00 -0300

    a

--- 09-Trabalhando com body-parser/app.js
+++ 09-Trabalhando com body-parser/app.js
@@ -1,12 +1,25 @@
 const express = require('express')
+// Body parser
+const bodyParser = require('body-parser')
 const app = express()
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 // especificando o diretório onde estão os arquivos estáticos
-app.use(express.static(__dirname + '/publico'))
+
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/pagina.html');
+  res.sendFile(__dirname + '/formulario.html');
   
+});
+app.post('/receber', (req, res) => {
+  
+  res.write('o nome e ' + req.body.nome);
+  res.end();
+
 })
+
 
 const port = 3000
 app.listen(port, () => {
