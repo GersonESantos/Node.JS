fc45807c6a946c77d63fdf0c0dbc80d68ae254a1
GersonESS <gebhsantos@gmail.com>
2025-02-10 18:47:09 -0300

    a

--- 15-Projeto Final (json-server)/front/app.js
+++ 15-Projeto Final (json-server)/front/app.js
@@ -13,6 +13,8 @@ app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.set('views', './views');
 
+app.use(express.static(__dirname + '/publico'));
+
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
--- 16 - Projeto Final (express, body-parser e express-handlebars)/front/app.js
+++ 16 - Projeto Final (express, body-parser e express-handlebars)/front/app.js
@@ -1,10 +1,20 @@
 var express = require('express');
-var expressHandlebars = require('express-handlebars');
+
+
+const { engine } = require('express-handlebars');
 var bodyParser = require('body-parser');
 
-
-// App
 var app = express();
+
+
+
+
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+
+app.use(express.static(__dirname + '/publico'));
+
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -12,17 +22,13 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
-app.engine('handlebars', expressHandlebars({defaultLayout : 'principal'}));
-app.set('view engine', 'handlebars');
-app.set('views', './views');
-
-// rotas
 app.get('/', (req, res) => {
     res.render('inicio');
 });
-// servidor
 
-app.listen(8080, () => {
-    console.log('Rodando app listening at http://localhost:8080');
-  });
+
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
+
 
