4dadcdc8a8e8456f62f700a838a7c9f5a84551c3
GersonESS <gebhsantos@gmail.com>
2025-11-25 14:12:20 -0300

    a

--- NodeJsGES/02/index.js
+++ NodeJsGES/02/index.js
@@ -3,12 +3,7 @@ const app = express()
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', (req, res) => {
-    // res.send('hello world German')
-    // res.send('<h1>Hello Gerson</h1>')
-    res.json({
-              title: 'Hello Word', 
-              message: 'This is a JSON response'
-             })
+  res.send('hello world German')
 })
 app.listen(3000, () => {
   console.log('Example app listening on port 3000!')
